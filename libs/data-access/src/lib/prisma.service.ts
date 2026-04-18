e664de33e3939808142cc38a302cfc08f599f0ba
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2026-04-18 21:47:37 +0200

    feat(libs): update data access and dto packages

--- libs/data-access/src/lib/prisma.service.ts
+++ libs/data-access/src/lib/prisma.service.ts
@@ -2,12 +2,22 @@ import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService implements OnModuleInit, OnModuleDestroy {
+  private readonly client = new PrismaClient();
+
+  get user() {
+    return this.client.user;
+  }
+
+  get message() {
+    return this.client.message;
+  }
+
   async onModuleInit(): Promise<void> {
-    await this.$connect();
+    await this.client.$connect();
   }
 
   async onModuleDestroy(): Promise<void> {
-    await this.$disconnect();
+    await this.client.$disconnect();
   }
 }
